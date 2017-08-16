7888215620348e9ddd038d5a05af7ef9234e7122
mcstitzer <mcstitzer@ucdavis.edu>
2017-08-15 21:10:43 -0700

    Create Exercise_1.4_WorkingDirectory.R

---
+++ day_1/Day_1_Exercises/Exercise_1.4_WorkingDirectory.R
@@ -0,0 +1,21 @@
+### Exercise 1.4 - getting data into R
+# useful commands setwd(), read.csv(), list.files(), getwd()
+
+### Questions ------------------------------------------------------------------
+    
+#* 01. Enter getwd() to determine your current working directory. 
+    
+# *02. Set working directory to the class folder you have from Google Drive 
+ 
+#* 03. List the files present in this directory.
+ 
+#* 04. Read "sleep.csv" into R, store as an object sleep
+  
+# 05. How many columns are in sleep?
+  
+# 06. Remove sleep from your R working session
+  
+# 07. Move up one working directory to the parent folder,
+#       list objects present, and then move back down to the course folder.
+       
+    
