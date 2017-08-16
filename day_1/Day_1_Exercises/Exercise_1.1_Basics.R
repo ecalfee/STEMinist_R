b5bde8f6b572db4bf4d7f9b2e293b4e88294b3b1
mcstitzer <mcstitzer@ucdavis.edu>
2017-08-15 20:53:00 -0700

    Create Exercise_1.1_Basics.R

---
+++ day_1/Day_1_Exercises/Exercise_1.1_Basics.R
@@ -0,0 +1,31 @@
+# Exercise 1.1 - interacting with R 
+# useful functions: c(), ls(), rm(), objects()
+
+### Questions-------------------------------------
+
+#* 01. Open Rstudio and perform an arithmetic calculation in the command line.
+
+
+#* 02. Create a numeric vector in the command line containing:
+
+        # the numbers 2, 9, 3, 8, and 3 and assign this vector to a global variable x. 
+
+        # Perform arithmetic with x. 
+
+        # Convince yourself R works as a calculator, and knows order of operations. 
+
+        # Multiply x by 10, and save the result as a new object named y
+        
+        # Calculate the difference in the sum of the x vector and the sum of the y vector
+
+
+# 03. Call the help files for the functions ls() and rm()
+
+       # What are the arguments for the ls() function?
+
+       # What does the 'sorted' argument do? 
+
+
+# 04. List all objects in the global environment. 
+
+      # Remove x and y from the global environment.
