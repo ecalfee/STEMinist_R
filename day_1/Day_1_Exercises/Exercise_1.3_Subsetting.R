c8ee9fed02d931b5168b836efd7b6ce108c0b75b
mcstitzer <mcstitzer@ucdavis.edu>
2017-08-15 21:07:37 -0700

    Create Exercise_1.3_Subsetting.R

---
+++ day_1/Day_1_Exercises/Exercise_1.3_Subsetting.R
@@ -0,0 +1,51 @@
+### Exercise 1.3 - indexing by logical statements
+# useful commands:  "==", "!=", ">", "<", "&", "|", sum(), which(), table(), !
+
+### Questions ------------------------------------------------------------------
+
+#* 1. Create your own logical vector with three TRUEs and three FALSEs
+
+
+#* 2. Produce a vector of the index number of the true’s
+
+
+#  3. Produce a second vector which indexes the numbers of the falses
+
+
+## go back to the iris dataset
+
+#  4. How many irises have sepals less than 5.5 cm?
+
+
+#* 5. Which iris individual has the largest petal length? What is the width of it's petal? 
+
+
+#  6.  How many of the irises are in this dataset belong to the species versicolor?
+
+
+#* 7. How many irises have petals longer than 6cm?
+
+
+#  8. Create a vector of species name for each iris with sepals longer than 6cm.
+
+
+#* 9. How many irises have sepals shorter than 5cm, but wider than 3cm?
+
+
+# 10. How many irises have petals narrower than 0.2cm or shorter than 1.5cm?
+
+
+# 11. What is the average width of setosa iris sepals that are longer than 5cm?
+
+
+# 12. What is the difference between the longest and shortest petal lengths of the species virginica?
+
+
+# 13. What proportion of flowers in the dataset have petals wider than 1cm?
+
+
+# 14. Create a new column within your dataframe, called sepalCategory, and set all values equal to 'long'
+#       Subset short values of this column, and set their values to 'short'
+#       (Short sepals are those less than 5.5 cm)
+#       How many plants with short sepals are there? How many long?
+
