c579507b9863bf0a24c0e920af39eb52976d5959
mcstitzer <mcstitzer@ucdavis.edu>
2017-08-15 20:56:27 -0700

    Create Exercise_1.2_Dataframes.R

---
+++ day_1/Day_1_Exercises/Exercise_1.2_Dataframes.R
@@ -0,0 +1,57 @@
+### Exercise 1.2 - characterize a data frame
+
+# useful commends: [row , col], str(), dim(), nrow(), unique(), length(), rownames(), summary(), min(), max(), 
+# mean(), range(), levels(), factor(), as.factor(), class(), ncol(), nrow(), table(), sum(), quantile(), var()
+
+## We'll use the built-in 'iris' dataset
+
+data(iris) # this loads the 'iris' dataset 
+
+# load help file for the 'iris' dataset
+?iris
+help(iris)
+
+# this calls the object iris
+iris 
+
+## If you're not a botanist, remind yourself what an iris looks like
+#  http://suruchifialoke.com/img/ML/iris.png
+
+### Questions ------------------------------------------------------------------
+
+#* 01. How many rows are in the dataset?
+
+
+#  02. What are three distinct ways to figure this out? 
+
+
+#* 03. How many species of flowers are in the dataset?
+
+
+# 04. What class is iris?
+
+
+#* 05. How many columns does this data frame have? What are their names?
+
+
+#* 06. What class did R assign to each column?
+
+
+# 07. Assign the first flower's petal's width and length to new objects called setosa1.petalwidth and setosa1.petallength
+
+
+#* 08. Calculate the approximate area of the petal of the first flower, setosa1 (assume petal area can be approximated by a rectangle).
+
+
+# 09. Calculate the petal area of each flower in the iris dataset
+#        and assign this to a new column named PetalArea.
+
+
+# 10. What is the maximum sepal length of the irises?
+
+
+#* 11. What is the average sepal length among flowers in the datset?
+
+
+# 12. How about the minimum and median sepal length? 
+
